0195c705fda03abdcee906d786274e6c48a3b366
cvijander <cvija85@gmail.com>
2024-08-22 17:55:46 +0200

    Cards

--- 1. Learning vue/app.js
+++ 1. Learning vue/app.js
@@ -1,7 +1,11 @@
 const app =Vue.createApp({
   data() {
       return {
-
+          deskName: 'Vue.js Fundamentals',
+          cards : cards ,
+          isFlipped : false,
+          readMoreUrl:"https://v3.vuejs.org/guide",
+          userInput:'default'
       }
   },
   methods: {
